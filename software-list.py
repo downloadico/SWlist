e3eab1b3d80bac333787845fae7f35f4efb6ac93
James Prewett <download@carc.unm.edu>
2023-01-09 16:51:38 -0700

    fixups, formatting is nicer now

--- software-list.py
+++ software-list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env spack-python
 
 import stackprinter
+from tabulate import tabulate
 import json
-from operator import attrgetter
 from itertools import groupby
 import os
 
@@ -27,15 +27,15 @@ def normalize_pkgs(pkg_lst):
     master = [] # accumulate packages here
     # produce a dict representing a package
     # with (at least) these keys: 'name', 'version', 'required', and 'module_name'
-    def normalize_pkg(pkg, master): 
+    def normalize_pkg(pkg, master):
         newpkg = {}
         newpkg['name'] = g(pkg, 'package')
-        if 'versionName' in g(pkg,'versions')[0]:
+        if 'versionName' in g(pkg, 'versions')[0]:
             newpkg['version'] = pkg['versions'][0]['versionName']
             if 'parent' in pkg['versions'][0]:
                 newpkg['required'] = pkg['versions'][0]['parent'][0][0]
             else:
-                newpkg['required'] = 'DEFAULT'
+                newpkg['required'] = '<None>'
             if 'versionName' in pkg['versions'][0]:
                 newpkg['version'] = pkg['versions'][0]['versionName']
                 newpkg['module_name'] = pkg['versions'][0]['full']
@@ -50,20 +50,14 @@ def info(item):
 def print_item(item):
     print(" ".join(info(item)))
 
-    
 def availableSW(table):
     srtTable = sorted(table, key=lambda x: g(x, 'required'))
-    for key, group in groupby(srtTable , lambda x: g(x, 'required')):
-        print("k: " + str(key))
-        if str(key) == '':
-            print("these modules are available by default:")
-        else:
-            print("these modules are only available after loading the " +
-                  str(key) + " module:")
-        for entry in  [{'name': 'name', 'version': 'version', 'required': 'required', 'module_name': 'module_name'}] + list(group):
-            print_item(entry)
+    TableHeaders = {'name': 'Software Name:',
+                    'version': 'Version:',
+                    'required': 'Prereq. modules:',
+                    'module_name': 'Module Name:'}
+    print(tabulate(srtTable, headers=TableHeaders))
 
-        
 def module_info_cmd():
     cmmd = ''.join([ os.environ['LMOD_DIR'],  "/spider" ,
                     ' '.join([' -o' , 'jsonSoftwarePage', os.environ['MODULEPATH']])])
