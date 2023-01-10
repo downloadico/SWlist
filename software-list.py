e0d724d237a6a5c5f6f0cb26696092e8e1cd97d0
James Prewett <download@carc.unm.edu>
2023-01-10 16:09:33 -0700

    made it get info from Spack (in addition to Lmod!)

--- software-list.py
+++ software-list.py
@@ -5,6 +5,7 @@ from tabulate import tabulate
 import json
 from itertools import groupby
 import os
+import yaml
 
 def g(thing, attr, default=[]):
     x = default
@@ -23,11 +24,20 @@ def version(thing):
 def module(thing):
     return(g(thing, 'module_name'))
 
-def normalize_pkgs(pkg_lst):
+def normalize_spackpkgs(pkg_lst):
+    master = [] # accumulate packages here
+    def spackage2module(spackage):
+        hash = spackage.hash
+        
+    def normalize_spackpkg(pkg, master):
+        newpkg = {}
+        newpkg['name'] = g(pkg, 'name')
+
+def normalize_spiderpkgs(pkg_lst):
     master = [] # accumulate packages here
     # produce a dict representing a package
     # with (at least) these keys: 'name', 'version', 'required', and 'module_name'
-    def normalize_pkg(pkg, master):
+    def normalize_spiderpkg(pkg, master):
         newpkg = {}
         newpkg['name'] = g(pkg, 'package')
         if 'versionName' in g(pkg, 'versions')[0]:
@@ -41,7 +51,7 @@ def normalize_pkgs(pkg_lst):
                 newpkg['module_name'] = pkg['versions'][0]['full']
             master.append(newpkg)
     for pkg in pkg_lst:
-        normalize_pkg(pkg, master)
+        normalize_spiderpkg(pkg, master)
     return(master)
 
 def info(item):
@@ -58,17 +68,59 @@ def availableSW(table):
                     'module_name': 'Module Name:'}
     print(tabulate(srtTable, headers=TableHeaders))
 
-def module_info_cmd():
-    cmmd = ''.join([ os.environ['LMOD_DIR'],  "/spider" ,
-                    ' '.join([' -o' , 'jsonSoftwarePage', os.environ['MODULEPATH']])])
-    return(cmmd)
-
 # run spider command
 def get_modules_info():
-    cmd = module_info_cmd()
+    # cmd = module_info_cmd()
+    cmd = ''.join([ os.environ['LMOD_DIR'],  "/spider" ,
+                    ' '.join([' -o' , 'jsonSoftwarePage', os.environ['MODULEPATH']])])
     stream = os.popen(cmd)
     output = json.loads(stream.read())
     return(output)
 
-packages = normalize_pkgs(get_modules_info())
-availableSW(packages)
+def get_spack_modules_conf():
+    cmd = 'spack config get modules'
+    stream = os.popen(cmd)
+    output = yaml.load(stream.read())
+    return(output)
+
+def spack_hashlength():
+    config = get_spack_modules_conf()
+    return(config['modules']['default']['lmod']['hash_length'])
+
+hash_length = spack_hashlength()
+
+def spack_find_json(args=''):
+    cmd = ' '.join(['spack',  'find', args, '--json'])
+    stream = os.popen(cmd)
+    output = json.loads(stream.read())
+    return(output)
+    
+
+def spackage2ModName(spackage):
+    modulename = '-'.join(["/".join([spackage['name'], spackage['version']]),
+                           spackage['hash'][0:hash_length]])
+    return(modulename)
+
+def normalizeSpackages(list):
+    master = [] # accumulate packages here
+    def normalizeSpackage(spackage, master):
+        # 'name', 'version', 'required', and 'module_name'
+        pkg = {}
+        pkg['name'] = spackage['name']
+        pkg['version'] = spackage['version']
+        pkg['required'] = ' '.join([ spackage['compiler']['name'], spackage['compiler']['version'] ])
+        pkg['module_name'] = spackage2ModName(spackage)
+        master.append(pkg)
+    
+    for pkg in list:
+        normalizeSpackage(pkg, master)
+        
+    return(master)
+
+print("output from spack:")
+spackPackages = normalizeSpackages(spack_find_json())
+availableSW(spackPackages)
+
+print("output from lmod:")
+spiderPackages = normalize_spiderpkgs(get_modules_info())
+availableSW(spiderPackages)
